c961ac10e69defdb573b9c4e9e494a7c95e94db1
brandon-kyle-bailey <brandon.bailey@produce8.com>
2024-07-30 14:15:25 -0300

    feat: some feature release test

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export function isEven(value: number): boolean {
   console.log("value is", value);
 
   console.log("this is a bug fix", value);
+  console.log("this is a feat", value);
   return value % 2 === 0;
 }
