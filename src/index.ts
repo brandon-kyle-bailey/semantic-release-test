02845fa6bb717906e69a315102050c4118bc7f9a
brandon-kyle-bailey <brandon.bailey@produce8.com>
2024-07-30 14:24:28 -0300

    BREAKING CHANGE: testing a breaking change

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export function isEven(value: number): boolean {
 
   console.log("this is a bug fix", value);
   console.log("this is a feat", value);
+  console.log("this is a breaking change", value);
   return value % 2 === 0;
 }
